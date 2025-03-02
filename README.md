fc4e1972b63da2c10fc7ca2aa786b3eb192fb4a5
elvin-mark <munoz.vega.em@gmail.com>
2025-03-02 22:14:49 +0900

    removing torch from requirements

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # startup.sh
 Simple startup script to initialize my dev environment
+
+```sh
+docker run -it --name my-alpine -p 3000:3000 -p 5000:5000 -p 8888:8888 alpine
+```
+
--- startup.sh
+++ startup.sh
@@ -8,3 +8,4 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 git clone https://github.com/elvin-mark/init.vim $HOME/.config/nvim
+pip3 install -r requirements.txt  --break-system-packages
