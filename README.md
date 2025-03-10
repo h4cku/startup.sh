55b9b0553c5a4e9b13fb1bedf94c7c0cb7fb0c3e
elvin-mark <munoz.vega.em@gmail.com>
2025-03-10 22:12:27 +0900

    adding script for ubuntu

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # startup.sh
 Simple startup script to initialize my dev environment
 
+## Alpine
 ```sh
 docker run -it --name my-alpine -p 3000:3000 -p 5000:5000 -p 8888:8888 alpine
 ```
@@ -8,3 +9,13 @@ docker run -it --name my-alpine -p 3000:3000 -p 5000:5000 -p 8888:8888 alpine
 ```sh
 docker exec -it my-alpine sh
 ```
+
+## Ubuntu
+```sh
+docker run -it --name my-ubuntu -p 3000:3000 -p 5000:5000 -p 8888:8888 ubuntu
+```
+
+```sh
+docker exec -it my-ubuntu bash
+```
+
---
+++ ubuntu/startup.sh
@@ -0,0 +1,15 @@
+apt update
+apt install git curl zsh make
+apt install ngrep netcat-openbsd socat
+apt install neovim ripgrep
+apt install nasm gcc gdb clang-format
+apt install python3 python3-pip golang npm nodejs
+
+mkdir -p $HOME/.config/nvim
+
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+git clone https://github.com/elvin-mark/init.vim $HOME/.config/nvim
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
