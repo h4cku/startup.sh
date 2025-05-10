a940944b0ebeb45e40f39e69c31b0bf43acbc407
elvin-mark <munoz.vega.em@gmail.com>
2025-05-10 16:09:13 +0900

    adding archlinux

---
+++ archlinux/startup.sh
@@ -0,0 +1,16 @@
+pacman -Syu git curl zsh make vim
+pacman -Syu ngrep socat inetutils nmap
+pacman -Syu neovim ripgrep
+pacman -Syu nasm gcc gdb clang
+pacman -Syu python python-pip go npm nodejs
+
+mkdir -p $HOME/.config/nvim
+
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+git clone https://github.com/elvin-mark/init.vim $HOME/.config/nvim
+
+pip3 install -r requirements.txt  --break-system-packages
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
